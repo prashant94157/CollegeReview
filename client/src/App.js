2d08aa139f330c5917bdfb167003b5beb17c8b91
prashant94157 <0166621m@gmail.com>
2023-07-29 23:37:48 +0530

    adding Spinner and React-redux

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,10 @@ import Homescreen from './screens/Homescreen';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
+import { Provider } from 'react-redux';
+import store from './store';
+import Alert from './components/Alert';
+import Spinner from './components/Spinner';
 
 function App() {
   const [mousePosition, setMousePosition] = useState({
@@ -37,29 +41,33 @@ function App() {
   };
 
   return (
-    <Router>
-      <div className='bg-[#181829] text-white font-sans'>
-        <motion.div
-          className='cursor'
-          variants={variants}
-          animate='default'
-          transition={{ duration: 1 }}
-        />
-        <div className='relative z-20 w-full'>
-          <Header />
-          <div className='min-h-[253px]'>
-            <Routes>
-              <Route path='/' element={<Homescreen component={Login} />} />
-              <Route
-                path='/register'
-                element={<Homescreen component={Register} />}
-              />
-            </Routes>
+    <Provider store={store}>
+      <Router>
+        <div className='bg-[#181829] text-white font-sans'>
+          <motion.div
+            className='w-[600px] h-[600px] rounded-full fixed left-0 top-0 z-10 bg-gradient-to-br to-[#647dee] from-[#7f53ac]'
+            variants={variants}
+            animate='default'
+            transition={{ duration: 1 }}
+          />
+          <div className='relative z-20 w-full'>
+            <Header />
+            <Alert />
+            <div className='min-h-[253px]'>
+              {/* <Spinner /> */}
+              <Routes>
+                <Route path='/' element={<Homescreen component={Login} />} />
+                <Route
+                  path='/register'
+                  element={<Homescreen component={Register} />}
+                />
+              </Routes>
+            </div>
+            <Footer />
           </div>
-          <Footer />
         </div>
-      </div>
-    </Router>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ client/src/components/Spinner.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Player } from '@lottiefiles/react-lottie-player';
+import first from '../assets/first.json';
+import second from '../assets/second.json';
+import third from '../assets/third.json';
+
+class Spinner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.player = React.createRef(); // initialize your ref
+    this.value = Math.floor(Math.random() * 3) + 1;
+    console.log(this.value);
+  }
+
+  render() {
+    return (
+      <Player
+        className='pt-6'
+        ref={this.player} // set the ref to your class instance
+        autoplay={true}
+        loop={true}
+        controls={true}
+        speed={0.5}
+        src={this.value === 2 ? first : this.value === 1 ? second : third}
+        style={{ height: '50%', width: '50%' }}
+      ></Player>
+    );
+  }
+}
+
+export default Spinner;
