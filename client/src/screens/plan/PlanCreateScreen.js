0dcfda277200899f22b3b921ebf0d67e459f506c
prashant94157 <0166621m@gmail.com>
2023-08-20 17:25:47 +0530

    nits

--- client/src/screens/plan/PlanCreateScreen.js
+++ client/src/screens/plan/PlanCreateScreen.js
@@ -104,7 +104,9 @@ const PlanCreateScreen = () => {
                 <select
                   id='planType'
                   value={planType}
-                  onChange={onChange}
+                  onChange={(e) =>
+                    setFormData({ ...formData, planType: e.target.value })
+                  }
                   class='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                 >
                   <option value='free'>Free</option>
